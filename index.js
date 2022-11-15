0cf0d0a61fe4609c8f70f173e92d304ea4317107
Mahad366 <mahad.younis@gigalabs.co>
2022-11-15 14:03:58 +0500

    header-transition

---
+++ index.js
@@ -0,0 +1,34 @@
+const sections = document.querySelectorAll("section");
+const navLi = document.querySelectorAll(".navbar .header-right a");
+const navbar = document.querySelector(".navbar");
+const body = document.querySelector("body");
+
+window.addEventListener("scroll", () => {
+  let current = "";
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    const sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("class");
+    }
+  });
+  navLi.forEach((a) => {
+    a.classList.remove("home-link");
+    if (a.classList.contains(current)) {
+      a.classList.add("home-link");
+    }
+  });
+});
+
+window.addEventListener("scroll", () => {
+  // if (pageYOffset > 400) {
+  //   navbar.style.backgroundColor = "#ffffff";
+  // }
+  // if (pageYOffset < 401) {
+  //   navbar.style.backgroundColor = "rgba(0, 0, 0, 0)";
+  // }
+
+  pageYOffset > 400
+    ? (navbar.style.backgroundColor = "#ffffff")
+    : (navbar.style.backgroundColor = "rgba(0, 0, 0, 0)");
+});
